0fd6abb2a97c171343e1b0c4eebf87681962e533
Igor Potashev <fil_igor@bk.ru>
2021-12-28 13:51:20 +0300

    fix links

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,15 +14,15 @@ function Footer({ children }) {
       <div className="basis-1/3 p-6">
         <ol>
           <li className="">
-            <a href="https://vk.com/mrgorre" className="text-slate-600 hover:text-slate-900" target={'_blank'}>
+            <a href="https://vk.com/MrGorre" className="text-slate-600 hover:text-slate-900" target={'_blank'}>
               <FaVk className="inline-block text-3xl m-2 align-middle" />
-              vk.com/mrgorre</a>
+              vk.com/MrGorre</a>
           </li>
           <li>
-            <a href="https://github.com/fily7" className=" text-slate-600 hover:text-slate-900" target={'_blank'}>
+            <a href="https://github.com/Mr-Gorre" className=" text-slate-600 hover:text-slate-900" target={'_blank'}>
               <FaGithub className=" inline-block text-3xl m-2 align-middle" />
               <span>
-                github.com/fily7
+              github.com/Mr-Gorre
               </span>
             </a>
           </li>
