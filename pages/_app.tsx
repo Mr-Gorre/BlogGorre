b8d3b14d49d809dcbd0ab0f66a6dc34a4ef4b195
Igor Potashev <fil_igor@bk.ru>
2021-12-29 15:58:45 +0300

    add yandex verifi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+      <meta name="yandex-verification" content="efe9e930e66cba53" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
