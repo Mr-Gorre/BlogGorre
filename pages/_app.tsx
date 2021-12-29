2215d5ddc589c5e93f8a34b1d49ee47af784f6f9
Igor Potashev <fil_igor@bk.ru>
2021-12-29 16:28:37 +0300

    add google verifi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
       <meta name="yandex-verification" content="efe9e930e66cba53" />
+      <script async  crossOrigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2293003440161756"></script>
       </Head>
       <Component {...pageProps} />
     </>
