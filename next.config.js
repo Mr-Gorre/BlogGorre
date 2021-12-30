0310bf7a25fb34167c591a17708016cf50d1741e
Igor Potashev <fil_igor@bk.ru>
2021-12-30 12:11:25 +0300

    add mdx

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [require("remark-prism")],
+  },
+})
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+})
