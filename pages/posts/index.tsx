b55e7ce0ee510f2c144602df98badc272cb25c8d
Igor Potashev <fil_igor@bk.ru>
2022-01-18 10:23:23 +0300

    add posts Array

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,16 +2,37 @@ import Link from "next/link";
 import React from "react";
 
 
+interface Post {
+  id: number,
+  title: string,
+  url: string,
+  description?: string,
+  publish_date?: Date
+}
+
 function Posts() {
+
+  const posts: Post[] = [
+    {
+      id: 1,
+      title: "Post 1",
+      url: "/posts/first-post"
+    },
+    {
+      id: 2,
+      title: "Post 2",
+      url: "/posts/about-first-post"
+    }
+  ]
+  const postList = posts.reverse().map((post) =>
+    <li key={post.id}>
+      <Link href={post.url}>{post.title}</Link>
+    </li>
+  )
   return(
     <>
       <ul className="text-center">
-        <li>
-          <Link href={'/posts/first-post'}>Пост 1</Link>
-        </li>
-        <li>
-          <Link href={'/posts/about-first-post'}>Пост 2</Link>
-        </li>
+        {postList}
       </ul>
     </>
   );
