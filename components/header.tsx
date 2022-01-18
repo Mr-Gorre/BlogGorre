886c5f722024a5ecd41cc3bc02078793ca196d89
Igor Potashev <fil_igor@bk.ru>
2022-01-18 16:04:52 +0300

    styling and text fix

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Nav from "./nav";
 
 function Header({children}) {
   return (
-    <header className="w-full text-center">
+    <header className="w-full text-center mb-10">
       <Nav></Nav>
       {children}
     </header>
